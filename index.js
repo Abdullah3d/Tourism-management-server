78cd7486a1d46281da04f3bfdfad4df28bb6d7c3
Abdullah3d <abdullah.ibrarhim.609@gmail.com>
2024-04-28 20:20:25 +0600

    added post function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -26,6 +26,35 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const touristCollection = client.db('touristDB').collection('tourist');
+
+    app.get('/tourist', async(req, res) => {
+      const cursor = touristCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
+    app.get('/tourist/:id', async(req, res) => {
+      const id= req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await touristCollection.findOne(query);
+      res.send(result);
+    })
+
+
+    app.post('/tourist', async(req, res)=> {
+      const newSpot = req.body;
+      console.log(newSpot);
+      const result = await touristCollection.insertOne(newSpot);
+      res.send(result);
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
